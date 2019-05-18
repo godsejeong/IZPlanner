bf34950eb7566f5767313147ace862cbe5aef3dd
godsejeong <dbdnpxm@naver.com>
2019-05-18 19:34:45 +0900

    update timeer

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 from flask import Flask
 from flask_restful import Resource, Api
 import PlanParser
@@ -6,20 +9,31 @@ import DetailsTimeParser
 app = Flask(__name__)
 api = Api(app)
 
+
 class RegistUser(Resource):
     def post(self):
-        return {'plan': PlanParser.plan()}
+        return {'plan': plan}
+
 
 class DetailPlan(Resource):
     def get(self, plan_name):
         print(plan_name)
-        detailJson = PlanParser.detailPlan()
-
         return {'detailPlan': detailJson[plan_name]}
 
 
 api.add_resource(DetailPlan, '/allPlanList/<string:plan_name>')
 api.add_resource(RegistUser, '/allPlanList')
 
+
+def start():
+    global plan
+    global detailJson
+    detailJson = PlanParser.detailPlan()
+    plan = PlanParser.plan()
+    print("start")
+    threading.Timer(2.5, start()).start()
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80,debug=True)
+    start()
+    app.run(host='0.0.0.0', port=80, debug=True)
